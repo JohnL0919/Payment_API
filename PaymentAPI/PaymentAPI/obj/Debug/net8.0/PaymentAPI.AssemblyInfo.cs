b60118571baf21341067fcb0bbb325387727e130
John <johnlin8968@gmail.com>
2024-07-03 21:45:29 +1000

    first commit

--- PaymentAPI/PaymentAPI/obj/Debug/net8.0/PaymentAPI.AssemblyInfo.cs
+++ PaymentAPI/PaymentAPI/obj/Debug/net8.0/PaymentAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac965e236094a3e340d3c8701dd6e67dc91cf8c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# Payment_API_Project
-This is a full-stack project using Asp.Net Core Web API CRUD with Angular 16.
-
-The goal of this project is to help me understand the flow of a full-stack application.
+"# Payment_Api" 
